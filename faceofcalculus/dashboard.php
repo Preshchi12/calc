c6cab0eb22351a1284deb399e9a3e097fa74d1c8
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-12-01 12:50:10 +0100

    Update dashboard.php
    added var_dump to prompt all var

--- faceofcalculus/dashboard.php
+++ faceofcalculus/dashboard.php
@@ -7,10 +7,15 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 <body>
-<h2>welcome <?php echo $_SESSION['full_name']; ?> to your dashboard</h2>
+    <?php
+   
+    var_dump($_SESSION);
+    ?>
+
+    <h2>Welcome <?php echo isset($_SESSION['full_name']) ? $_SESSION['full_name'] : 'User'; ?> to your dashboard</h2>
 
 </body>
 </html>
